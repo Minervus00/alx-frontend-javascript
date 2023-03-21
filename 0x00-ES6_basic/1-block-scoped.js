09c59de8db4a1076bb252f02b1eef5bcd8d54036
Minervus00 <fayouedraogo@protonmail.com>
2023-03-21 11:16:32 +0000

    test_1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,14 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  var task;
-  var task2;
-  // task = false;
-  // task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    var task = true;
-    var task2 = false;
+    task = false;
+    task2 = true;
   }
-  task = false;
-  task2 = true;
+
   return [task, task2];
 }
