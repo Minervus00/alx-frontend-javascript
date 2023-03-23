f978a92c7b46bde2786ccccf0c8cd77ae28c7b9b
Minervus00 <fayouedraogo@protonmail.com>
2023-03-23 10:34:56 +0000

    task3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,1 +1,11 @@
+import { uploadPhoto, createUser } from './utils';
 
+export default function handleProfileSignup() {
+  const prom1 = uploadPhoto();
+  const prom2 = createUser();
+  Promise.all([prom1, prom2])
+    .then((values) => {
+      console.log(values[0].body, ...Object.values(values[1]));
+    })
+    .catch(() => console.log('Signup system offline'));
+}
