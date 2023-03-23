51b9d5bf1a05fa23799c5259bf2c24e3d908c995
Minervus00 <fayouedraogo@protonmail.com>
2023-03-23 10:51:09 +0000

    task3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,6 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   const prom1 = uploadPhoto();
   const prom2 = createUser();
+
   return Promise.all([prom1, prom2])
     .then((values) => {
       console.log(values[0].body, ...Object.values(values[1]));
