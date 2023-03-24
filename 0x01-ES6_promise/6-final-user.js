730564cfe700baef484c2ca0ec03002f8c1927dc
Minervus00 <fayouedraogo@protonmail.com>
2023-03-24 10:42:11 +0000

    task6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,9 +1,10 @@
-import signUpUser from './4-user-promise'
-import uploadPhoto from './5-photo-reject'
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const user = signUpUser(firstName, lastName);
   const photo = uploadPhoto(fileName);
 
-  return Promise.allSettled([user, photo]);
+  return Promise.allSettled([user, photo])
+    .then((value) => value);
 }
