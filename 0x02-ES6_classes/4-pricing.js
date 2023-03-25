fce402e65eef24f4b6adf0a35d0857c76ce355d2
Minervus00 <fayouedraogo@protonmail.com>
2023-03-25 00:31:42 +0000

    task4

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,7 +1,23 @@
 export default class Pricing {
   constructor(amount, currency) {
     this._amount = amount;
-    this.currency = currency;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(val) {
+    this._amount = val;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(curr) {
+    this._currency = curr;
   }
 
   displayFullPrice() {
